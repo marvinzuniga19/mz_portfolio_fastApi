fae70866b6afcc9f9af175e81aea2e0213655178
Marvin <marvin_zuniga19@outlook.com>
2025-07-07 14:52:33 -0600

    changes

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,10 @@
-VANTA.NET({
-    el: "#vanta-canvas",
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: 200.00,
-    minWidth: 200.00,
-    scale: 1.00,
-    scaleMobile: 1.00,
-    color: 0xadb4c8,
-    backgroundColor: 0xf0720,
-    points: 14.00
+VANTA.RINGS({
+  el: "#vanta-canvas",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00
 })
