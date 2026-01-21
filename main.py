40927a367bba7c5a08c76278bbb102681d539507
marvin <marvin_zuniga19@outlook.com>
2026-01-21 15:28:49 -0600

    update cards

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ projects = [
     },
 {
         "title": "Web Resume",
-        "description": "One-page web résumé project for Marvin Zúniga. Simple, clean, and easy to navigate.",
+        "description": "One-page web resume project for Marvin Zúniga. Simple, clean, and easy to navigate.",
         "technologies": ["HTML", "CSS", "Javascript"],
         "github_url": "https://github.com/marvinzuniga19/Mz-Resume",
         "image": "/images/resume.png"
@@ -28,49 +28,49 @@ projects = [
     },
     {
         "title": "Python Web Scraper",
-        "description": "Desktop application developed with Python to compress images",
+        "description": "Web scraping tool built with Python, Beautiful Soup, and Requests to extract data from websites.",
         "technologies": ["Python", "Beautiful Soup", "Requests"],
         "github_url": "https://github.com/marvinzuniga19/flask_webJob_scraper",
         "image": "/images/webscraper.png"
     },
     {
         "title": "YES OR NO, Flutter Mobile App",
-        "description": "Desktop application developed with Python to compress images",
+        "description": "Mobile application developed with Flutter that answers yes or no to your questions",
         "technologies": ["Flutter", "Dart"],
         "github_url": "https://github.com/marvinzuniga19/Yes_no_app",
         "image": "/images/chat.png"
     },
     {
         "title": "Youtube Music Flet Downloader",
-        "description": "Desktop application developed with Python to compress images",
+        "description": "Desktop application developed with Python to download music from Youtube",
         "technologies": ["Python", "Flet"],
         "github_url": "https://github.com/marvinzuniga19/youtube_music_downloader",
         "image": "/images/youtubedownload.png"
     },
     {
         "title": "News Blog with Flask",
-        "description": "Desktop application developed with Python to compress images",
+        "description": "Web application developed with Flask that allows users to read and post news articles",
         "technologies": ["Python", "Flask"],
         "github_url": "https://github.com/marvinzuniga19/news_blog_with_Flask",
         "image": "/images/blog.png"
     },
     {
         "title": "Invoice Generator Desktop App",
-        "description": "Desktop application developed with Python to compress images",
+        "description": "Desktop application developed with Python to generate invoices in PDF format",
         "technologies": ["Python", "Flet", "ReportLab"],
         "github_url": "https://github.com/marvinzuniga19/invoice_Builder",
         "image": "/images/invoice.png"
     },
     {
         "title": "Task Manager - CRUD App with Flutter",
-        "description": "Desktop application developed with Python to compress images",
+        "description": "Mobile application developed with Flutter to manage tasks with CRUD operations",
         "technologies": ["Flutter", "Dart"],
         "github_url": "https://github.com/marvinzuniga19/Flutter_Crud_App",
         "image": "/images/crud.png"
     },
     {
         "title": "Image Compressor app with Flutter",
-        "description": "Desktop application developed with Python to compress images",
+        "description": "Mobile application developed with Flutter to compress images",
         "technologies": ["Flutter", "Dart"],
         "github_url": "https://github.com/marvinzuniga19/image_compressor_app",
         "image": "/images/compress.jpg"
